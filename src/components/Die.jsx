d1b246134e5e65c60928bb47a15535413028d597
Christina Wu <xchristinawu@gmail.com>
2022-08-16 16:56:50 -0700

    style: replace numbers with dice

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,15 +1,21 @@
 export default function Die(props) {
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF"
+        backgroundColor: props.isHeld ? "#404040" : "#FFFFFF"
     }
-    
+    const pipStyles = {
+        backgroundColor: props.isHeld ? "#FFFFFF" : "#404040"
+    }
+    const pips = Array
+                    .from(Array(props.value))
+                    .map((item,i) => <span key={i} className="pip" style={pipStyles}/>)
+
     return (
         <div 
             className="die" 
             style={styles} 
             onClick={props.holdDice}
         >
-            <p className="die-num">{props.value}</p>
+            <div className="face">{pips}</div>
         </div>
     )
 }
